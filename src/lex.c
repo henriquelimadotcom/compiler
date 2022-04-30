a5562dec4a3a9c40fb2095e693b2aa77d051c518
LowC0ff <henriquelima.dev@gmail.com>
2022-04-30 07:23:03 -0300

    Melhorando Função Proximo Token

--- src/lex.c
+++ src/lex.c
@@ -7,6 +7,17 @@
 //variável que passará por cada caractere do arquivo
 static int c;
 
+void makeToken(char *nome, int val) //mostrar o token
+{
+    printf("<%s, %d>", nome, val);
+}
+
+void voltaPonteiro() // volta um caracter se necessário
+{
+    if(c != EOF)
+        fseek(file, ftell(file)-1, SEEK_SET);
+}
+
 void proximo_token()
 {
     while (1)
@@ -16,7 +27,13 @@ void proximo_token()
         if (c == EOF)
             break;
         
-        else
-            printf("%c", c);
+        else if (c == ' ' || c == '\t')
+            continue;
+        
+        else if (c == '\n')
+        {
+            linha++;
+            continue;
+        }
     }
 }
