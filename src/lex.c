533e3f9e986f842aa582d369c5b657b18fee2de9
LowC0ff <henriquelima.dev@gmail.com>
2022-04-30 08:00:35 -0300

    Tratando Caracteres

--- src/lex.c
+++ src/lex.c
@@ -24,28 +24,90 @@ void proximo_token()
     {
         c = fgetc(file);
 
-        if (c == EOF)
+        if (c == EOF){
             break;
+        }
 
-        else if (c == '+')
+        else if (c == '+'){
             makeToken("+", 0);
+        }
 
-        else if (c == '-')
+        else if (c == '-'){
             makeToken("-", 0);
+        }
 
-        else if (c == '*')
+        else if (c == '*'){
             makeToken("*", 0);
+        }
 
-        else if (c == '/')
+        else if (c == '/'){
             makeToken("/", 0);
-        
-        else if (c == ' ' || c == '\t')
+        }
+
+        else if (c == '('){
+            makeToken("(", 0);
+        }
+
+        else if (c == ')'){
+            makeToken(")", 0);
+        }
+
+        else if (c == ':')
+        {
+            c = fgetc(file); //pega o proximo caractere
+
+            if (c == '=') //se for '=' sabemos que é o token ':='
+                makeToken(":=", 0);
+        }
+
+        else if (c == '<')
+        {
+            c = fgetc(file); //pega o proximo caractere
+
+            if (c == '=') //se for '=' sabemos que é o token '<='
+                makeToken("<=", 0);
+
+        else
+            makeToken("<", 0);
+        }
+
+        else if (c == '>')
+        {
+            c = fgetc(file);
+
+            if (c == '=')
+                makeToken(">=", 0);
+            
+            else 
+                makeToken(">", 0);
+        }
+
+        else if (c == '='){
+            makeToken("=", 0);
+        }
+
+        else if (isdigit(c)){
+            numero();
+        }
+
+        else if (isalpha(c)){
+            palavra();
+        }
+
+        else if (c == ' ' || c == '\t'){
             continue;
+        }
         
         else if (c == '\n')
         {
             linha++;
             continue;
+        }
+
+        else
+        {
+            printf("Caracter '%c' na linha %d não reconhecido.\n", c, linha);
+            exit(EXIT_FAILURE);
         }
     }
 }
